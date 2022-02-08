074696d8439eed634b7a704ead29b971e088c804
adeankush3 <adeankush3@gmail.com>
2022-02-08 18:35:00 +0530

    [Ankush]JSON file Deserialized

---
+++ InventoryDataManagement/InventoryDataManagement/InventoryManagement.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace InventoryDataManagement
+{
+    public class InventoryManagement
+    {
+        public void ReadData(string filepaath)
+        {
+            try
+            {
+                using (StreamReader streamReder = new StreamReader(filepaath))
+                {
+                    var json =streamReder.ReadToEnd();
+                    var item = JsonConvert.JsonSerializer.Serialize< List < Inventory >> (json);
+                    var options = new JsonSerializerOptions { WriteIndented = true };
+                    string jsonString = JsonSerializer.Serialize(InventoryManagement, options);
+
+                    Console.WriteLine(jsonString);
+                    Console.WriteLine("Name\t" + "Price\t" + "Weight\t" + "Total amount");
+                    foreach (var data in item)
+                    {
+                        Console.WriteLine(data.Name + "\t" + data.Price + "\t" + data.Weight + "\t" + data.Price * data.Weight);
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+            
+        }
+    }
+}
